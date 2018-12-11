76210357b04873a6b7d9035d2f8601fbc227fba9
KU Lok Sun <sundaykuloksun@gmail.com>
2018-12-11 18:14:51 +0800

    feat: split file into streams before parallel upload

--- parallelUploadAsync.js
+++ parallelUploadAsync.js
@@ -8,8 +8,24 @@ const uploadAsyncFactory = ({region, bucket}) => {
     params: { Bucket: bucket }
   });
   const uploadAsync = async (fileName, filePath, partSize) => {
-    const stats = fs.statSync(filePath);
-    return stats;
+    const { size: fileSize } = fs.statSync(filePath);
+    const partNum = Math.ceil(fileSize / partSize);
+
+    // Split a file into several file streams
+    const partStreams = [];
+    for (let i = 0; i < partNum; ++i) {
+      const fileStream = fs.createReadStream(
+        filePath,
+        { start: i * partSize, end: partSize }
+      );
+      const tag = `[Part ${i+1}]`;
+      fileStream.on("error", err => {
+        console.log(`${tag} Create Read Stream Error`);
+        throw err;
+      });
+      partStreams.push(fileStream);
+    }
+    return partStreams;
   }
   return uploadAsync;
 }
